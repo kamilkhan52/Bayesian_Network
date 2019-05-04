04cf8d7779c9613136d81bf680bb41f3a61584af
Kamil <kamilkhan52@outlook.com>
2019-05-04 15:03:30 -0600

    Removed print statements

--- Main.py
+++ Main.py
@@ -13,14 +13,11 @@ k = 0
 while k < len(lines_list) - 1:
     index = k
     num_parents = int(lines_list[index + 1][0])
-    # print("num_parents = " + str(num_parents))
     k = index + 2 ** int(num_parents) + 1
     child_id = int(lines_list[index + 1][1])
     if num_parents != 0:
         parents_dict[child_id] = lines_list[index + 1][2:]
-        # read the condition probabilities
         prob_table = lines_list[index + 2:k + 1]
-        # print("prob_table = " + str(prob_table))
         for row in prob_table:  # write each row into cond_prob_table
             all_parents = '_'.join(parents_dict[child_id])
             truth_table = '_'.join(row[:len(row) - 1])
@@ -47,22 +44,15 @@ for i in range(0, len(query_list)):
     if cond == 'T':
         key = str(i + 1) + "_T"
         if parents_dict[i + 1] == 0:
-            # print("Multiplying joint_prob = " + str(joint_prob) + " by cond_prob_table[key] =" + str(
-            #     cond_prob_table[key]))
             joint_prob = joint_prob * cond_prob_table[key]
-            # print("New joint_prob = " + str(joint_prob))
         elif len(parents_dict[i + 1]) > 0:
-            # print("parents count = " + str(len(parents_dict[i + 1])))
             child = i + 1
             parents = '_'.join(parents_dict[child])
             conditions = ""
             for parent in parents_dict[child]:
                 conditions = conditions + "_" + str(query_list[int(parent) - 1])
             read_key = str(child) + "_" + str(parents) + str(conditions)
-            # print("Multiplying joint_prob = " + str(joint_prob) + " by cond_prob_table[read_key] =" + str(
-            #     cond_prob_table[read_key]))
             joint_prob = joint_prob * cond_prob_table[read_key]
-            # print("New joint_prob = " + str(joint_prob))
 
         else:
             print("Unknown number of parents for this child" + str(i + 1))
@@ -70,23 +60,16 @@ for i in range(0, len(query_list)):
         key = str(i + 1) + "_F"
 
         if parents_dict[i + 1] == 0:
-            # print("Multiplying joint_prob = " + str(joint_prob) + " by cond_prob_table[key] =" + str(
-            #     cond_prob_table[key]))
             joint_prob = joint_prob * cond_prob_table[key]
-            # print("New joint_prob = " + str(joint_prob))
         elif len(parents_dict[i + 1]) > 0:
-            # print("parents count = " + str(len(parents_dict[i + 1])))
             child = i + 1
             parents = '_'.join(parents_dict[child])
             conditions = ""
             for parent in parents_dict[child]:
                 conditions = conditions + "_" + str(query_list[int(parent) - 1])
             read_key = str(child) + "_F_" + str(parents) + str(conditions)
-            # print("Multiplying joint_prob = " + str(joint_prob) + " by cond_prob_table[read_key] =" + str(
-            #     cond_prob_table[read_key]))
             joint_prob = joint_prob * cond_prob_table[read_key]
 
-            # print("New joint_prob = " + str(joint_prob))
         else:
             print("Unknown number of parents for this child" + str(i + 1))
 
